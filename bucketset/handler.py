3473bf3aba14153aef3dea612c32293d4b282b7a
Alex Ellis <alexellis2@gmail.com>
2017-11-01 17:26:58 +0000

    Add bucket-set function
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

---
+++ bucketset/handler.py
@@ -0,0 +1,29 @@
+from minio import Minio
+from minio.error import ResponseError
+import json
+import os
+
+# Request
+# { 
+#   "inbox": "aellis_catjump_inbox",
+#   "outbox": "aellis_catjump_outbox"
+# }
+def handle(st):
+    req = json.loads(st)
+    mc = Minio(os.environ['minio_url'],
+                    access_key=os.environ['minio_access_key'],
+                    secret_key=os.environ['minio_secret_key'],
+                    secure=False)
+    try:
+        res = mc.make_bucket(req["inbox"] , location='us-east-1')
+    except ResponseError as err:
+        print(err)
+    try:
+        res = mc.make_bucket(req["outbox"] , location='us-east-1')
+    except ResponseError as err:
+        print(err)
+
+# Response
+# Empty - success
+# Non-empty - failure
+
